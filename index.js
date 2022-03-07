34d20640cf9b1b51171772bc0813fec16b14e55a
veenasahu12 <veenasahusbp@gmail.com>
2022-03-07 12:55:18 +0530

    evaluation submit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+app.use(logger);
+
+app.get("/books",(req,res)=>{
+    return res.send({route:"/books"});
+});
+
+app.get("/libraries ",checkpermission("librarian"),(req,res)=>{
+    return res.send({ route: "/libraries", permission: true});
+});
+
+app.get("/authors",checkpermission("author"),(req,res)=>{
+    return res.send({ route: "/authors", permission: true});
+});
+
+
+function checkpermission(permission){
+    return function logger(req,res,next){
+        if(permission === "librarian"){
+            return next();
+        }
+        else if(permission === "author"){
+            return next();
+        }
+        else{
+            return res.send("not allowed");
+        }
+    }
+}
+function logger(req,res,next){
+    if(req.path === "/books"){
+        req.permission = "books";
+    }
+    else if(req.path === "/libraries"){
+        req.permission = "libraries";
+    }
+    else{
+        req.permission = "authors";
+    }
+    next();
+}
+
+app.listen(6000,() =>{
+    console.log("listening on port 6000");
+})
